7a37021fc111b7e34270a67d532dbefba942a130
ch4mpy <ch4mp@c4-soft.com>
2023-06-12 17:26:42 -1000

    openid-training deployment

---
+++ openid-training/openid-training.sh
@@ -0,0 +1,3 @@
+kubectl create namespace openid-training
+kubectl create secret generic openid-training-secrets -n openid-training --from-literal=mobile-front-secret=change-me --from-literal=web-back-secret=change-me --from-literal=web-front-secret=change-me
+kubectl apply -f openid-training.yml
